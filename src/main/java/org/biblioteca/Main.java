de9eec49b09f58a63d51bddecc24a127d25d21dc
Bruna Sonda <bsonda@gmail.com>
2025-06-03 11:57:05 +0200

    build: connect database

--- src/main/java/org/biblioteca/Main.java
+++ src/main/java/org/biblioteca/Main.java
@@ -1,7 +1,9 @@
 package org.biblioteca;
 
+import org.biblioteca.config.DBManager;
+
 public class Main{
     public static void main (String[]args){
-        System.out.println("Hola mundo");
+        DBManager.initConnection();
     }
 }
--- src/main/java/org/biblioteca/config/DBManager.java
+++ src/main/java/org/biblioteca/config/DBManager.java
@@ -1,4 +1,24 @@
 package org.biblioteca.config;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBManager {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/library";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    private static Connection connection;
+
+    public static Connection initConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexion exitosa");
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return connection;
+    }
+
 }
