2fca6cec4b4f5acc96d3b7ce33fc2ab51e1a0a42
Mariya Byehan <maria_byehan@yahoo.com>
2025-06-03 15:00:52 +0200

    feat: add option list

--- src/main/java/org/biblioteca/Main.java
+++ src/main/java/org/biblioteca/Main.java
@@ -1,11 +1,14 @@
 package org.biblioteca;
 
 import org.biblioteca.config.DBManager;
+import org.biblioteca.view.BookView;
 
 public class Main{
     public static void main (String[]args){
         DBManager.initConnection();
+        BookView view = new BookView();
+        view.viewMenu();
         DBManager.closeConnection();
-
     }
+
 }
